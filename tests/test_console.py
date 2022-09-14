a8cafe7855c41800568f7dd746fdc9f2a96bf101
doloadama <doloadama@outlook.com>
2022-09-14 16:14:26 +0000

    .

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,47 +1,45 @@
 #!/usr/bin/python3
-"""Test Console"""
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
+"""
+Contains the class TestConsoleDocs
+"""
+
+import console
+import inspect
+import pep8
 import unittest
+HBNBCommand = console.HBNBCommand
 
 
-class TestConsole(unittest.TestCase):
-    """
-    This test checks if all required classes
-    are created correctly.
-    """
-    def test_class(self):
-        """
-        This test checks if all required classes
-        are present
-        """
-        city1 = City()
-        amenity1 = Amenity()
-        state1 = State()
-        rev1 = Review()
-        place1 = Place()
-        self.assertEqual(city1.__class__.__name__, "City")
-        self.assertEqual(amenity1.__class__.__name__, "Amenity")
-        self.assertEqual(state1.__class__.__name__, "State")
-        self.assertEqual(rev1.__class__.__name__, "Review")
-        self.assertEqual(place1.__class__.__name__, "Place")
+class TestConsoleDocs(unittest.TestCase):
+    """Class for testing documentation of the console"""
+    def test_pep8_conformance_console(self):
+        """Test that console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_father(self):
-        """
-        This test checks if all required classes
-        inherit correcly from BaseModel
-        """
-        city1 = City()
-        amenity1 = Amenity()
-        state1 = State()
-        rev1 = Review()
-        place1 = Place()
-        self.assertTrue(issubclass(city1.__class__, BaseModel))
-        self.assertTrue(issubclass(amenity1.__class__, BaseModel))
-        self.assertTrue(issubclass(state1.__class__, BaseModel))
-        self.assertTrue(issubclass(rev1.__class__, BaseModel))
-        self.assertTrue(issubclass(place1.__class__, BaseModel))
+    def test_pep8_conformance_test_console(self):
+        """Test that tests/test_console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_console_module_docstring(self):
+        """Test for the console.py module docstring"""
+        self.assertIsNot(console.__doc__, None,
+                         "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
+
+    def test_HBNBCommand_class_docstring(self):
+        """Test for the HBNBCommand class docstring"""
+        self.assertIsNot(HBNBCommand.__doc__, None,
+                         "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand class needs a docstring")
+
+
+if __name__ == '__main__':
+    unittest.main
