a54b93650756f8547aa9541051c4db73f2f608da
doloadama <doloadama@outlook.com>
2022-09-14 15:43:02 +0000

    .

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-base class
+base class file
 """
 
 import uuid
@@ -8,67 +8,59 @@ from datetime import datetime
 import models
 from json import JSONEncoder
 
+
 class BaseModel:
     """
+    =========
     BaseModel
+    =========
     """
- def __init__(self, *args, **kwargs):
-        """Base Model __init__ Method
-        Here, the default values of a Base Model
-        instance are initialized.
-        """
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.updated_at = datetime.now()
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        if len(kwargs) > 0:
+
+    def __init__(self, *args, **kwargs):
+        """initialize the instance of the class"""
+        if kwargs:
             for key, value in kwargs.items():
-                """loop through the key and its corresponding value"""
                 if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, time_format)
-                    """we convert to dictionary key with the
-                    given value of the time string
-                    """
-                else:
-                    if key != "__class__":
-                        self.__dict__[key] = value
-                        """if key is not found in __class__ , we must add
-                        the value to
-                        this dictionary with the object's class name
-                        """
+                    value = datetime.strptime(
+                            value,
+                            '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "__class__":
+                    continue
+
+                setattr(self, key, value)
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
-    def __str__(self):
-        """Representation of the class for the user
-        Example:
-            $ bm = BaseModel()
-            $ print(bm)
-            This method prints the content of the Base Model
-            class with this format
-            $ [<class name>] (<self.id>) <self.__dict__>
-        """
-        return '[{}] ({}) {}'.format(
-                self.__class__.__name__, self.id, self.__dict__
-            )
-
     def save(self):
-        """Updates a Base Model instance
-        Updates the public instance attribute `updated_at`
-        with the current datetime and dumps the class data
-        into a file
-        """
+        """save new informations to the class object"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Converts the information of the class to human-readable format
-        Returns a new dictionary containing all keys/values
-        of __dict__ of the instance.
-        """
-        dict_n = self.__dict__.copy()
-        dict_n['__class__'] = self.__class__.__name__
-        dict_n['created_at'] = self.created_at.isoformat()
-        dict_n['updated_at'] = self.updated_at.isoformat()
+        """return dictionary representaton of the instance"""
+        dict_repr = {}
+        for key, value in self.__dict__.items():
+            dict_repr[key] = value
+            if isinstance(value, datetime):
+                dict_repr[key] = value.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        dict_repr["__class__"] = type(self).__name__
+        return dict_repr
 
-        return dict_n
+    def __str__(self):
+        """return the string formated message when instance is called"""
+        clName = self.__class__.__name__
+        return "[{}] ({}) {}".format(clName, self.id, self.__dict__)
+
+
+class BaseModelEncoder(JSONEncoder):
+    """JSON Encoder for BaseModel
+    """
+
+    def default(self, o):
+        """ default"""
+        if isinstance(o, BaseModel):
+            return o.to_dict()
+        return super().default(o)
